c7e2d6cb9d754da8eab47aa734f74d3e2cb79464
Heeral Shah <heeralashah@gmail.com>
2022-08-17 22:39:25 +0100

    set up H_matrix, S_matrix, CNOT_matrix, CNOT_tensor and Reg class

---
+++ qrmcircuit_code.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy.linalg import norm
+
+H_matrix = 1/np.sqrt(2) * np.array([[1, 1],
+                                    [1, -1]])
+
+S_matrix = np.array([[1, 0],
+                    [0, 0+ 1j]])
+
+#print(S_matrix.imag)
+
+CNOT_matrix = np.array([[1,0,0,0],
+                        [0,1,0,0],
+                        [0,0,0,1],
+                        [0,0,1,0]])
+                    
+
+CNOT_tensor = np.reshape(CNOT_matrix, (2,2,2,2))
+
+class Reg:
+    def __init__(self, n):
+        self.n = n
+        self.psi = np.zeros((2,) *n )
+        self.psi[(0,) *n ] = 1
+
+
+
+
+
+#main_circ = np.zeros((2,2,2))
+
+#print (main_circ)
+#print(main_circ.size)
+
+
+
+#print(CNOT_tensor)
