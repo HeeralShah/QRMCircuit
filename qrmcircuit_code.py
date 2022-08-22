827c8c53067117914562122f5c69a704f2ca3667
Heeral Shah <heeralashah@gmail.com>
2022-08-22 22:08:23 +0100

    added qubit_measure method to the Reg class which measures a single specified qubit and returns the register post measurement, as well as setting a value in a binary list stored as the measure instance variable of the Reg class.

--- qrmcircuit_code.py
+++ qrmcircuit_code.py
@@ -39,6 +39,7 @@ class Reg:
         self.n = n
         self.psi = np.zeros((2,) *n )
         self.psi[(0,) *n ] = 1
+        self.measure = [None] * n
     
     def one_qubit_op(self, operator, i):
         self.psi = np.tensordot(operator, self.psi, (1, i))
@@ -61,18 +62,38 @@ class Reg:
             self.one_qubit_op(operator, i)
             i+=1
         return self
+
+    def qubit_reset(self, i):
+        return None
+    #    move = np.moveaxis(self.psi, 0, i)
+    #    move[(0,) * self.n ] = 1
+    #   move[(1, 0,) * (self.n -1 )]
+
     
     def reg_reset(self):
         self.psi = np.zeros((2,) *self.n )
-        self.psi[(0,) *self.n ] = 1
+        self.psi[(0,) * self.n ] = 1
         return self
 
     def project(self, i, j): # RETURN state with ith qubit of reg projected onto |j>
         self.psi = np.moveaxis(np.tensordot(projectors[j], self.psi, (1, i)), 0, i)
         return self
     
+    def measure_qubit(self, i):
 
+        projected = self.project(i, 0)
+        norm_projected = norm(projected.psi.flatten())
 
+        if np.random.random() < norm_projected ** 2:
+            self.psi = projected.psi / norm_projected
+            self.measure[i] = 0
+        else:
+            projected = self.project(i, 0)
+            norm_projected = norm(projected.psi.flatten())
+            self.psi = projected.psi / norm_projected
+            self.measure[i] = 1
+        
+        return self
 
 
 
@@ -80,23 +101,38 @@ class Reg:
 
 ##################################  TESTING   #########################################
 
-reg = Reg(4)
-#print(reg.psi)
 
-#final_reg = reg.transversal(reg.H)
-med_reg = reg.transversal(H_matrix)
-final_reg = reg.transversal(S_matrix)
+i=0
+counter = 0
+while i<100:
 
-print(final_reg.psi)
+    reg = Reg(4)
 
-reset_reg = final_reg.reg_reset()
+    #print(reg.measure)
+    #print(reg.psi)
 
-print(reset_reg.psi)
+    #final_reg = reg.transversal(reg.H)
+    med_reg = reg.transversal(H_matrix)
+    final_reg = reg.transversal(S_matrix)
+
+    #print(final_reg.psi)
+
+
+
+    reset_reg = final_reg.measure_qubit(2).measure
+    
+    #print(reset_reg)
+    if reset_reg[2] == 1:
+        counter +=1
+    
+    i+=1
+
+print("the value of the counter is:" + str(counter))
 
 reg2 = Reg(3)
 reg2 = reg2.one_qubit_op(X_matrix, 2)
-print(reg2.psi)
+#print(reg2.psi)
 
 reg2.CX_op(2, 0)
 reg2.CZ_op(0, 2)
-print(reg2.psi)
+#print(reg2.psi)
